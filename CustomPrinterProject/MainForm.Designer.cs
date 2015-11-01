a1f8063f290c23f00a078f43acb75cee5cf06f9a
rajgopal-naramsetty <rajgopal.nnv@gmail.com>
2015-11-01 14:27:03 +0530

    Changes to Custom Project

---
+++ CustomPrinterProject/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CustomPrinterProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPrinter = new System.Windows.Forms.ComboBox();
+            this.lblSelectPrinter = new System.Windows.Forms.Label();
+            this.btnCheckStatus = new System.Windows.Forms.Button();
+            this.rtbDetails = new System.Windows.Forms.RichTextBox();
+            this.btnReadMagData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbPrinter
+            // 
+            this.cbPrinter.FormattingEnabled = true;
+            this.cbPrinter.Location = new System.Drawing.Point(163, 52);
+            this.cbPrinter.Name = "cbPrinter";
+            this.cbPrinter.Size = new System.Drawing.Size(301, 21);
+            this.cbPrinter.TabIndex = 0;
+            // 
+            // lblSelectPrinter
+            // 
+            this.lblSelectPrinter.AutoSize = true;
+            this.lblSelectPrinter.Location = new System.Drawing.Point(28, 60);
+            this.lblSelectPrinter.Name = "lblSelectPrinter";
+            this.lblSelectPrinter.Size = new System.Drawing.Size(70, 13);
+            this.lblSelectPrinter.TabIndex = 1;
+            this.lblSelectPrinter.Text = "Select Printer";
+            // 
+            // btnCheckStatus
+            // 
+            this.btnCheckStatus.Location = new System.Drawing.Point(503, 47);
+            this.btnCheckStatus.Name = "btnCheckStatus";
+            this.btnCheckStatus.Size = new System.Drawing.Size(185, 35);
+            this.btnCheckStatus.TabIndex = 2;
+            this.btnCheckStatus.Text = "Check Status";
+            this.btnCheckStatus.UseVisualStyleBackColor = true;
+            this.btnCheckStatus.Click += new System.EventHandler(this.btnCheckStatus_Click);
+            // 
+            // rtbDetails
+            // 
+            this.rtbDetails.Location = new System.Drawing.Point(31, 422);
+            this.rtbDetails.Name = "rtbDetails";
+            this.rtbDetails.Size = new System.Drawing.Size(657, 325);
+            this.rtbDetails.TabIndex = 3;
+            this.rtbDetails.Text = "";
+            // 
+            // btnReadMagData
+            // 
+            this.btnReadMagData.Location = new System.Drawing.Point(503, 105);
+            this.btnReadMagData.Name = "btnReadMagData";
+            this.btnReadMagData.Size = new System.Drawing.Size(185, 35);
+            this.btnReadMagData.TabIndex = 4;
+            this.btnReadMagData.Text = "Read Magnetic Data";
+            this.btnReadMagData.UseVisualStyleBackColor = true;
+            this.btnReadMagData.Click += new System.EventHandler(this.btnReadMagData_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 759);
+            this.Controls.Add(this.btnReadMagData);
+            this.Controls.Add(this.rtbDetails);
+            this.Controls.Add(this.btnCheckStatus);
+            this.Controls.Add(this.lblSelectPrinter);
+            this.Controls.Add(this.cbPrinter);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPrinter;
+        private System.Windows.Forms.Label lblSelectPrinter;
+        private System.Windows.Forms.Button btnCheckStatus;
+        private System.Windows.Forms.RichTextBox rtbDetails;
+        private System.Windows.Forms.Button btnReadMagData;
+    }
+}
+
---
+++ CustomPrinterProject/MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fargo.PrinterSDK;
+using System.Management;
+using System.Threading;
+
+namespace CustomPrinterProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cbPrinter.Items.Clear();
+            cbPrinter.Items.AddRange(GetPrinters());
+        }
+
+        private string[] GetPrinters()
+        {
+            ManagementObjectCollection printerCollection =
+                new ManagementObjectSearcher("SELECT * FROM Win32_Printer").Get();
+
+            var printers = new ArrayList();
+
+            foreach (var obj in printerCollection)
+            {
+                var printer = (ManagementObject)obj;
+                printers.Add(printer.Properties["Name"].Value.ToString());
+            }
+
+            return (string[])printers.ToArray(typeof(string));
+        }
+
+        private void btnCheckStatus_Click(object sender, EventArgs e)
+        {
+            var objPrinterInfo = new PrinterInfo(cbPrinter.SelectedItem.ToString());
+
+            var printerData = new StringBuilder();
+            printerData.Append("SerialNo:" + objPrinterInfo.SerialNumber + "\n");
+            printerData.Append("Details:" + objPrinterInfo.ToString() + "\n");
+
+            rtbDetails.Text = printerData.ToString();
+        }
+
+        private void btnReadMagData_Click(object sender, EventArgs e)
+        {
+            var objMag = new MagneticStripeCard(cbPrinter.SelectedItem.ToString());
+            string track1, track2, track3;
+
+            var movement = new Movement(cbPrinter.SelectedItem.ToString());
+            movement.MoveTo(Station.Magnetic, 0);
+
+            // Wait until the mag read is done to the firmware.  Was there an error in printer?
+            if (WaitForMagReadFinish(cbPrinter.SelectedItem.ToString()))
+            {
+                objMag.GetMagneticData(out track1, out track2, out track3);
+                rtbDetails.Text = String.Format("Track1:{0}\nTrack2:{1}\nTrack3:{2}", track1, track2, track3);
+            }
+        }
+
+        /// <summary>
+        /// Wait for the mag data to be finished being read from the encoder to the firmware.
+        /// </summary>
+        /// <returns>TRUE if mag data is ready to be read from the printer</returns>
+        private bool WaitForMagReadFinish(string printerName)
+        {
+            // Assume that a failure will occur.
+            bool result = false;
+            StationStatus stationStatus;
+            int m_timeout = 120000; // 2 minutes.
+            bool bContinue = true;
+            CurrentActivity currentActivity;
+            int nTimer = Environment.TickCount;
+
+            var printerInfo = new PrinterInfo(printerName);
+            string driverName = GetDriverName(printerName);
+            
+
+
+            // For HDP8500 can monitor to see when the mag data has been fully read to the printers buffer.
+            if (driverName.Contains("HDP8500"))
+            {
+                //// Because the firmware does not update the Mag StationStatus to CardNotPresent until the card is ejected we can not
+                //// rely on that to tell the mag data is finished on second and later cards.
+                //result = true;
+
+                do
+                {
+                    currentActivity = printerInfo.CurrentActivity();
+
+                    switch (currentActivity)
+                    {
+                        // Feed error?
+                        case CurrentActivity.CurrentActivityException:
+                            bContinue = false;
+                            break;
+
+                        default:
+                            // Read to see if the station status indicates present BEFORE reading mag data.
+                            stationStatus = printerInfo.StationStatus(Station.Magnetic);
+
+                            // If card present then ok to read the data.
+                            if (stationStatus == StationStatus.CardPresent)
+                            {
+                                result = true;
+                                bContinue = false;
+                            }
+                            else
+                            {
+                                // Wait for next poll.
+                                Thread.Sleep(500);
+                                bContinue = true;
+                            }
+
+                            break;
+                    }
+
+                } while (((Environment.TickCount - nTimer) < m_timeout) && bContinue);
+
+            }
+
+            // For testing with HDP5000.
+            else if (driverName.Contains("HDP5000"))
+            {
+                do
+                {
+                    currentActivity = printerInfo.CurrentActivity();
+
+                    switch (currentActivity)
+                    {
+                        case CurrentActivity.CurrentActivityWaitingForEject:
+                            // If the printer is holding the card then it is done.
+                            // Sleep for a few seconds to make sure that the data has been read.
+                            Thread.Sleep(5000);
+                            result = true;
+                            bContinue = false;
+                            break;
+                        case CurrentActivity.CurrentActivityException:
+                            bContinue = false;
+                            break;
+                        default:
+                            Thread.Sleep(1000);
+                            bContinue = true;
+                            break;
+                    }
+
+                } while (((Environment.TickCount - nTimer) < m_timeout) && bContinue);
+
+            }
+
+
+            // Must be Neo / Onyx model
+            else
+            {
+                do
+                {
+                    currentActivity = printerInfo.CurrentActivity();
+
+                    switch (currentActivity)
+                    {
+                        case CurrentActivity.CurrentActivityEncodeMagStrip:
+                            // The DTC printers don't support getting the mag station status.
+                            // Sleep for a few seconds to make sure that the data has been read.
+                            Thread.Sleep(5000);
+                            result = true;
+                            bContinue = false;
+                            break;
+                        case CurrentActivity.CurrentActivityException:
+                            bContinue = false;
+                            break;
+                        default:
+                            Thread.Sleep(1000);
+                            bContinue = true;
+                            break;
+                    }
+
+                } while (((Environment.TickCount - nTimer) < m_timeout) && bContinue);
+            }
+
+
+            int howLong = (Environment.TickCount - nTimer);
+
+            // Do something to breakpoint on.
+            if (result == false)
+            {
+                if ((Environment.TickCount - nTimer) >= m_timeout)
+                {
+                }
+            }
+
+            return (result);
+        }
+
+        private string GetDriverName(string strPrinterName)
+        {
+            string retValue = string.Empty;
+
+
+            //handle backslashes in network printer names
+            if (strPrinterName.Contains("\\\\") == true)
+                strPrinterName = strPrinterName.Replace("\\", "\\\\");
+
+            //handle apostrophes in printer names
+            if (strPrinterName.Contains("'") == true)
+                strPrinterName = strPrinterName.Replace("'", "\\'");
+
+            string searchQuery = string.Format("Select * FROM Win32_Printer Where Name = '{0}'", strPrinterName);
+            ManagementObjectSearcher searchPrinters =
+                new ManagementObjectSearcher(searchQuery);
+            ManagementObjectCollection printerCollection = searchPrinters.Get();
+
+            foreach (ManagementObject obj in printerCollection)
+            {
+                retValue = obj.Properties["DriverName"].Value.ToString();
+                break;
+            }
+
+            return retValue;
+
+        }
+
+    }
+}
